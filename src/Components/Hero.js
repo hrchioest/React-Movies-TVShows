24cb4183f4ed94a56486f77335efdf264d142c04
dolores <dperezlaborda@gmail.com>
2020-07-22 13:15:49 -0300

    se crea branch de peliculas

---
+++ src/Components/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Hero.scss';
+
+const Hero = () =>{
+    return(
+        <div>
+            <div className="circulo"></div>
+        </div>
+    )
+}
+
+export default Hero; 
---
+++ src/Components/Movies.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Movies.scss';
+
+const Movies = () =>{
+    return(
+        <div className="container">
+        </div>
+    )
+}
+
+export default Movies; 

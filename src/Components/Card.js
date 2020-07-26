e2fbf1824f94094265402feedd30e7fd26f3476e
Giuli <giulicecchetto@gmail.com>
2020-07-26 16:13:37 -0300

    css agregado

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import movie from "../movie.jpg";
 import "../App.scss";
 
 const Card = () => {
     return (
         <div id="card">
             <div className="img-card">
-                <img src={movie} alt="img" />
             </div>
             <h3 className="card-title">Blade Runner</h3>
         </div>

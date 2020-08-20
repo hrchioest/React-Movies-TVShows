92d3984ad3fe7424489f8bcaefd333a110c09741
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-08-20 11:11:40 -0300

    Se agrega los listados de tv

--- src/sections/ListAll.jsx
+++ src/sections/ListAll.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {useParams} from "react-router-dom";
   
 import ListCard from './ListCard'
-import { movieTrending, trendingTv, moviePopular, movieTopRated, movieUpcoming, movieNow } from "../service/index";
+import { movieTrending, trendingTv, moviePopular, movieTopRated, movieUpcoming, movieNow, tvPopular, tvTopRated, tvNow} from "../service/index";
 
 const sections = {
     movie: {
@@ -28,13 +28,24 @@ const sections = {
     movieNow:{
         api:movieNow,
         title:"Películas en cines"
+    },
+    tvPopular:{
+        api:tvPopular,
+        title:"Series populares"
+    },
+    tvTopRated:{
+        api:tvTopRated,
+        title:"Series con más críticas"
+    },
+    tvNow:{
+        api:tvNow,
+        title:"Series al aire"
     }
 }
 
 const ListAll = () =>{
 
     const { seccion } = useParams();
-
 
     return(
         <>  

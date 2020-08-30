b52b010837fdc2645e311f3d222ef9c3e590058c
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-08-29 21:29:24 -0300

    se cambia de ruta para hacer la paginacion

--- src/service/index.js
+++ src/service/index.js
@@ -2,7 +2,7 @@
 export const movieTrending = "https://api.themoviedb.org/3/trending/movie/week?api_key=b28a6d7a756aac4ded04e3b860e94284";
 export const trendingTv = "https://api.themoviedb.org/3/trending/tv/week?api_key=b28a6d7a756aac4ded04e3b860e94284";
 
-export const moviePopular = "https://api.themoviedb.org/3/movie/popular?api_key=b28a6d7a756aac4ded04e3b860e94284";
+export const moviePopular = "https://api.themoviedb.org/3/movie/popular?api_key=5dd9a436e42ee145f42a0361aceafad8&language=es-US&page=1";
 export const movieTopRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=b28a6d7a756aac4ded04e3b860e94284";
 export const movieUpcoming = "https://api.themoviedb.org/3/movie/upcoming?api_key=b28a6d7a756aac4ded04e3b860e94284";
 export const movieNow = "https://api.themoviedb.org/3/movie/now_playing?api_key=b28a6d7a756aac4ded04e3b860e94284";

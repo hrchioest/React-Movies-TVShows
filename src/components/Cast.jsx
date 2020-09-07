7d91e43048a04b678666534688ffea3bd9834c61
Giuli <giulicecchetto@gmail.com>
2020-09-07 12:54:07 -0300

    info de cast traida de api finalizada

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -4,29 +4,37 @@ import axios from "axios";
 
 const Cast = ({ id }) => {
 
+
     const [cast, setCast] = useState([]);
 
     useEffect(() => {
         axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=b28a6d7a756aac4ded04e3b860e94284&language=es`)
             .then(res => {
-                setCast(res.data.results);
+                setCast(res.data.cast);
             }).catch(error => console.log(error))
     }, []);
 
     useEffect(() => console.log(cast), [cast]);
 
+
+    let castList;
+    if (cast) {
+        castList = cast.map((c, i) => {
+            return (
+                <li key={i}>
+                    <span>{c.name} - </span>
+                    <span>{c.character}</span>
+                </li>
+            );
+        });
+    }
+
+
     return (
         <div>
-            <ul>
-                {cast.map((cast, i) => {
-                    return (
-                        <li key={i}>
-                            <span>{cast.name}</span>
-                            <span>{cast.character}</span>
-                        </li>
-                    )
-                })}
-            </ul>
+                <ul>
+                    {castList}
+                </ul>
         </div>
     )
 }
--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -6,7 +6,7 @@ import StarScore from './StarScore';
 
 const MovieInfo = ({ id, title, poster, stars, overview, length, genres, budget, revenue, production }) => {
 
-    const [externalIds, setExternalIds] = useState([]);
+  /*  const [externalIds, setExternalIds] = useState([]);
 
     useEffect(() => {
         axios.get(`https://api.themoviedb.org/3/movie/${id}/external_ids?api_key=b28a6d7a756aac4ded04e3b860e94284&language=es`)
@@ -17,7 +17,7 @@ const MovieInfo = ({ id, title, poster, stars, overview, length, genres, budget,
 
     useEffect(() => console.log(externalIds), [externalIds]);
 
-
+*/
     let genresList;
     if (genres) {
         genresList = genres.map((g, i) => {

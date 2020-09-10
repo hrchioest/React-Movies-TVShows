85257c012d074be607d025017040af0fdb6c1f3e
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-09-10 16:22:23 -0300

    para conectar a la api

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "../sass/components/card.scss";
+import {Link} from "react-router-dom";
+import info from "./MovieInfo";
+
 
 
 const Card = props => {
 
+  
   return (
     <div id='card'>
       <div className='img-card'>
-        <img src={'https://image.tmdb.org/t/p/w370_and_h556_bestv2/' + props.image} alt="poster"/>
+        <Link to ={`/${info.id}/info`}><img src={'https://image.tmdb.org/t/p/w370_and_h556_bestv2/' + props.image} alt="poster"/></Link>
       </div>
       <h3 className='card-title'>{props.title}</h3>
     </div>

f9cd54a77a6c6f22e5df1a16336485bfac861a54
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-09-17 14:27:40 -0300

    se hace el llamado de la api desde el componente Videos y se pasa por parámetros al componente video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,35 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import YouTube from "react-youtube";
-import axios from "axios";
-import { videoMovie } from "../service/index";
-import { useParams } from "react-router-dom";
 
 import "../sass/components/video.scss";
 
-const Video = ({ id }) => {
-  const [trailer, setTrailer] = useState([]);
-  useEffect(() => {
-    axios
-      .get(videoMovie.replace("[id]", id))
-      .then((res) => {
-        setTrailer(res.data.results);
-      })
-      .catch((error) => console.log(error));
-  }, [id]);
-
+const Video = ({ id, name, type }) => {
   const opts = {
     height: "214px",
     width: "375px"
   };
-
   return (
     <div className='video'>
       <div className='content-youtube'>
-        <YouTube className='youtube' videoId={trailer.id} opts={opts} />
+        <YouTube className='youtube' videoId={id} opts={opts} />
       </div>
       <div className='content-titles'>
-        <h2 className='title'>{trailer.name}</h2>
-        <h3 className='subtitle'>{trailer.type}</h3>
+        <h2 className='title'>{name}</h2>
+        <h3 className='subtitle'>{type}</h3>
       </div>
     </div>
   );

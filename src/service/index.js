8fe1124c1fa93cb72e148e2dbffa2a21d277238f
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-09-16 22:33:03 -0300

    se reemplaza endpoint de external de redes sociales como constante en index

--- src/service/index.js
+++ src/service/index.js
@@ -1,17 +1,27 @@
+export const movieTrending =
+  "https://api.themoviedb.org/3/trending/movie/week?api_key=b28a6d7a756aac4ded04e3b860e94284";
+export const trendingTv =
+  "https://api.themoviedb.org/3/trending/tv/week?api_key=b28a6d7a756aac4ded04e3b860e94284";
 
-export const movieTrending = "https://api.themoviedb.org/3/trending/movie/week?api_key=b28a6d7a756aac4ded04e3b860e94284";
-export const trendingTv = "https://api.themoviedb.org/3/trending/tv/week?api_key=b28a6d7a756aac4ded04e3b860e94284";
+export const moviePopular =
+  "https://api.themoviedb.org/3/movie/popular?api_key=b28a6d7a756aac4ded04e3b860e94284";
+export const movieTopRated =
+  "https://api.themoviedb.org/3/movie/top_rated?api_key=b28a6d7a756aac4ded04e3b860e94284";
+export const movieUpcoming =
+  "https://api.themoviedb.org/3/movie/upcoming?api_key=b28a6d7a756aac4ded04e3b860e94284";
+export const movieNow =
+  "https://api.themoviedb.org/3/movie/now_playing?api_key=b28a6d7a756aac4ded04e3b860e94284";
 
-export const moviePopular = "https://api.themoviedb.org/3/movie/popular?api_key=b28a6d7a756aac4ded04e3b860e94284";
-export const movieTopRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=b28a6d7a756aac4ded04e3b860e94284";
-export const movieUpcoming = "https://api.themoviedb.org/3/movie/upcoming?api_key=b28a6d7a756aac4ded04e3b860e94284";
-export const movieNow = "https://api.themoviedb.org/3/movie/now_playing?api_key=b28a6d7a756aac4ded04e3b860e94284";
+export const tvPopular =
+  "https://api.themoviedb.org/3/tv/popular?api_key=b28a6d7a756aac4ded04e3b860e94284";
+export const tvTopRated =
+  "https://api.themoviedb.org/3/tv/top_rated?api_key=b28a6d7a756aac4ded04e3b860e94284";
+export const tvNow =
+  "https://api.themoviedb.org/3/tv/on_the_air?api_key=b28a6d7a756aac4ded04e3b860e94284";
 
-export const tvPopular = "https://api.themoviedb.org/3/tv/popular?api_key=b28a6d7a756aac4ded04e3b860e94284";
-export const tvTopRated = "https://api.themoviedb.org/3/tv/top_rated?api_key=b28a6d7a756aac4ded04e3b860e94284";
-export const tvNow = "https://api.themoviedb.org/3/tv/on_the_air?api_key=b28a6d7a756aac4ded04e3b860e94284";
-
-
-export const info = "https://api.themoviedb.org/3/[type]/[id]?api_key=5dd9a436e42ee145f42a0361aceafad8&language=es";
-export const cast = "https://api.themoviedb.org/3/[type]/[id]/credits?api_key=5dd9a436e42ee145f42a0361aceafad8&language=es";
-export const externalIds = "https://api.themoviedb.org/3/[type]/[id]/external_ids?api_key=b28a6d7a756aac4ded04e3b860e94284&language=es";
+export const info =
+  "https://api.themoviedb.org/3/[type]/[id]?api_key=5dd9a436e42ee145f42a0361aceafad8&language=es";
+export const cast =
+  "https://api.themoviedb.org/3/[type]/[id]/credits?api_key=5dd9a436e42ee145f42a0361aceafad8&language=es";
+export const external =
+  "https://api.themoviedb.org/3/[type]/[id]/external_ids?api_key=b28a6d7a756aac4ded04e3b860e94284&language=es";

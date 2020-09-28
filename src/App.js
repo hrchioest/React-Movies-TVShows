98356a3f27002b4021b19dec2d47f95b91383715
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-09-28 16:01:04 -0300

    Revert "Merge pull request #27 from hrchioest/peliculas"
    This reverts commit 7d4b00e24dd5eb03b405e571fcfcc4970fd07e6d, reversing
    changes made to 9de01afc55fc4deccc3606153c817036a353b4e6.

--- src/App.js
+++ src/App.js
@@ -4,26 +4,20 @@ import Movie from "./sections/Movie";
 import Tv from "./sections/Tv";
 import ListAll from "./sections/ListAll";
 import ContainerInfo from "./components/ContainerInfo";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Search from "./sections/Search";
 
-// import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import Header from "./sections/Header";
 
 function App() {
+
   return (
     <Router>
       <div className='App'>
-        <Header />
+        <Search/>
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/movie' component={Movie} />
           <Route exact path='/tv' component={Tv} />
-          <Route
-            exact
-            path='/multi/:wordSearch/page/:numPage'
-            component={Search}
-          />
           <Route exact path='/:seccion/trending' component={ListAll} />
           <Route path='/:type/:id' component={ContainerInfo} />
         </Switch>
--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,36 +1,39 @@
 import React from "react";
-import "../sass/sections/header.scss";
-import SearchInput from "../components/SearchInput";
+import '../sass/sections/header.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { faVideo } from "@fortawesome/free-solid-svg-icons";
 import { faTv } from "@fortawesome/free-solid-svg-icons";
-import { NavLink } from "react-router-dom";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ handleInput, search }) => {
+
   return (
     <nav id='header-nav'>
       <div className='access-nav'>
         <ul>
           <li>
-            <NavLink to='/' exact activeClassName='active-access'>
+            <NavLink to="/" exact activeClassName="active-access" >
               <FontAwesomeIcon className='icons-nav' icon={faHome} />
             </NavLink>
           </li>
           <li>
-            <NavLink to='/movie' exact activeClassName='active-access'>
+            <NavLink to="/movie" exact activeClassName="active-access" >
               <FontAwesomeIcon className='icons-nav' icon={faVideo} />
             </NavLink>
           </li>
           <li>
-            <NavLink to='/tv' exact activeClassName='active-access'>
+            <NavLink to="/tv" exact activeClassName="active-access" >
               <FontAwesomeIcon className='icons-nav' icon={faTv} />
             </NavLink>
           </li>
         </ul>
       </div>
-
-      <SearchInput />
+      <div className='searcher-nav'>
+        <FontAwesomeIcon className='icons-nav' icon={faSearch} />
+        <input type='text' placeholder='Búsqueda...' onChange={handleInput} onKeyPress={search}/> 
+      </div>
     </nav>
   );
 };

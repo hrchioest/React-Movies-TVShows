1a07bd1133e0a55a32bd47ef0ee6d11525940378
Rocio del Pilar Esteban <hr.chio@gmail.com>
2020-10-19 22:50:31 -0300

    Se agrega en index la variable de page para cada enpoint de movies y series, y se agrega page en los link por cada listCard

--- src/sections/Tv.jsx
+++ src/sections/Tv.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React from "react";
 import ListCard from "../sections/ListCard";
 
 import { tvPopular, tvTopRated, tvNow } from "../service/index";
 
 const Tv = () => {
-
-
-    return(
-        <>
-            <ListCard title="Series populares" limit={5} api={tvPopular} type="tv" link ="/tvPopular/trending" />
-            <ListCard title="Series con más críticas" limit={5} api={tvTopRated} type="tv" link ="/tvTopRated/trending" />
-            <ListCard title="Series al aire" limit={5} api={tvNow} type="tv" link ="/tvNow/trending" />
-        </>
-    )
-}
+  return (
+    <>
+      <ListCard
+        title='Series populares'
+        limit={5}
+        api={tvPopular}
+        type='tv'
+        link='/tvPopular/trending/page/1'
+      />
+      <ListCard
+        title='Series con más críticas'
+        limit={5}
+        api={tvTopRated}
+        type='tv'
+        link='/tvTopRated/trending/page/1'
+      />
+      <ListCard
+        title='Series al aire'
+        limit={5}
+        api={tvNow}
+        type='tv'
+        link='/tvNow/trending/page/1'
+      />
+    </>
+  );
+};
 
 export default Tv;
--- src/service/index.js
+++ src/service/index.js
@@ -1,16 +1,16 @@
 const api_key = process.env.REACT_APP_API_KEY_THEMOVIEDB;
 
-export const movieTrending = `https://api.themoviedb.org/3/trending/movie/week?api_key=${api_key}`;
-export const trendingTv = `https://api.themoviedb.org/3/trending/tv/week?api_key=${api_key}`;
+export const movieTrending = `https://api.themoviedb.org/3/trending/movie/week?api_key=${api_key}&language=es-US&page=[page]`;
+export const trendingTv = `https://api.themoviedb.org/3/trending/tv/week?api_key=${api_key}&language=es-US&page=[page]`;
 
-export const moviePopular = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key} `;
-export const movieTopRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key} `;
-export const movieUpcoming = `https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key} `;
-export const movieNow = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}`;
+export const moviePopular = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=es-US&page=[page]`;
+export const movieTopRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}&language=es-US&page=[page]`;
+export const movieUpcoming = `https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=es-US&page=[page]`;
+export const movieNow = `https://api.themoviedb.org/3/movie/now_playing?api_key=${api_key}&language=es-US&page=[page]`;
 
-export const tvPopular = `https://api.themoviedb.org/3/tv/popular?api_key=${api_key}`;
-export const tvTopRated = `https://api.themoviedb.org/3/tv/top_rated?api_key=${api_key}`;
-export const tvNow = `https://api.themoviedb.org/3/tv/on_the_air?api_key=${api_key}`;
+export const tvPopular = `https://api.themoviedb.org/3/tv/popular?api_key=${api_key}&language=es-US&page=[page]`;
+export const tvTopRated = `https://api.themoviedb.org/3/tv/top_rated?api_key=${api_key}&language=es-US&page=[page]`;
+export const tvNow = `https://api.themoviedb.org/3/tv/on_the_air?api_key=${api_key}&language=es-US&page=[page]`;
 
 export const info = `https://api.themoviedb.org/3/[type]/[id]?api_key=${api_key}&language=es`;
 export const cast = `https://api.themoviedb.org/3/[type]/[id]/credits?api_key=${api_key}&language=es`;
